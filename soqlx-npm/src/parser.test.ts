2d92ff01f858795f23b34e12dc4e19e9f5ed2f09
stephenlf <stephenlfunk@gmail.com>
2024-04-24 15:45:06 -0400

    More tests

--- soqlx-npm/src/parser.test.ts
+++ soqlx-npm/src/parser.test.ts
@@ -2,14 +2,36 @@ import parse, { parseParamFileArg, SoqlxParserError } from './parser';
 import { describe, expect, test } from '@jest/globals'
 
 describe('parseParamFileArg module', () => {
-    test('good input', () => {
+    test('good input: with comment', () => {
         const arg = "'./src/index.ts' is a valid file path"
         const filePath = parseParamFileArg(arg);
         expect(filePath).toBe('./src/index.ts');
     });
 
+    test('good input: with spaces and comment', () => {
+        const arg = "'./test-data/This is a file.txt' This file should work";
+        const filePath = parseParamFileArg(arg);
+        expect(filePath).toBe('./test-data/This is a file.txt');
+    })
+
+    test('good input: double quotes', () => {
+        const arg = "\"./test-data/This is a file.txt\"";
+        const filePath = parseParamFileArg(arg);
+        expect(filePath).toBe('./test-data/This is a file.txt');  
+    });
+
+    test('bad data: empty string', () => {
+
+    });
+
     test('bad output: file doesn\'t exist', () => {
         const arg = "./invalid.invalid";
+        expect(() => {parseParamFileArg(arg)})
+            .toThrow(SoqlxParserError);
+    });
+
+    test('bad output: path is dir', () => {
+        const arg = "./src";
         expect(() => {parseParamFileArg(arg)})
             .toThrow(SoqlxParserError);
     });
